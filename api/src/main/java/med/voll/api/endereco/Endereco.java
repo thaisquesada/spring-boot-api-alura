bf7f54d79acda50f06f50175c12fa094b60b4b3b
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2022-12-20 11:43:00 -0300

    entidades JPA 🐱‍💻

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,24 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}
---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,29 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.endereco.Endereco;
+import org.hibernate.annotations.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "Medico")
+@Table(name = "medicos")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+}
