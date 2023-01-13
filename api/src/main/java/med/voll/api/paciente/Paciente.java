4d09ee84a69d706278362aa616a4cc9b338b6c46
Thais Quesada <101607673+thaisquesada@users.noreply.github.com>
2023-01-13 15:27:01 -0300

    inclusão da atualização de dados de pacientes 🐱‍💻

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -23,12 +23,23 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
-
     public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.email() != null) {
+            this.email = dados.email();
+        }
+        if (dados.email() != null) {
+            this.email = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
     }
 }
